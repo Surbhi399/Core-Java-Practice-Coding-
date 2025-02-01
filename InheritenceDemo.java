a7e1287c15910965ddf5952f1821c8158d3fc6de
SURBHI JAIN <121885461+Surbhi399@users.noreply.github.com>
2025-02-01 20:51:57 +0530

    InheritenceDemo
    Example of Inheritance

---
+++ InheritenceDemo.java
@@ -0,0 +1,24 @@
+public class InheritenceDemo {
+    public static void main(String[] args) {
+        Friend f = new Friend();
+
+    }}
+
+    class You
+    {
+        private int acc;
+    You()
+    {
+        acc = 12345;
+       
+
+    }
+
+    }
+class  Friend extends You
+{
+       Friend(){
+           super();
+       }
+}
+
