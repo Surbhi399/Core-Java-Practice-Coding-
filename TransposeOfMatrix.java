9eca5b430ad2e8c4279a37a519a3d507fc6b25fb
SURBHI JAIN <121885461+Surbhi399@users.noreply.github.com>
2025-02-01 21:06:56 +0530

    TransposeOfMatrix.java
    Updated Code for Transpose of Matrix

--- TransposeOfMatrix.java
+++ TransposeOfMatrix.java
@@ -1,64 +1,55 @@
 import java.util.Scanner;
 
 public class Transpose {
-    public static int[][] transpose(int[][] A) {
-    int numRows =A.length;
-    int numCols = A[0].length;
+    
+    // Method to transpose a matrix
+    public static int[][] transpose(int[][] matrix) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+        int[][] transposedMatrix = new int[numCols][numRows];
 
-    // Create a new matrix to store the transposed values
-    int [][] transposedMatrix =  new int[numCols][numRows];
-
-        for(int i = 0;i<numRows;i++)
-
-    {
-        for (int j = 0; j < numCols; j++) {
-            transposedMatrix[j][i] = A[i][j];
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                transposedMatrix[j][i] = matrix[i][j];
+            }
         }
+        return transposedMatrix;
     }
 
-        return transposedMatrix;
-}
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Input matrix dimensions
+            System.out.print("Enter the number of rows: ");
+            int rows = scanner.nextInt();
+            System.out.print("Enter the number of columns: ");
+            int cols = scanner.nextInt();
 
-
-    public static void main(String[] args)
-    {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter the number of rows: ");
-        int row = s.nextInt();
-        System.out.print("Enter the number of columns: ");
-        int  column = s.nextInt();
-      int[][] A = new int[row][column];
-        System.out.println("Enter matrix elements:");
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < A[i].length; j++) {
-
-                A[i][j] = s.nextInt();
-
-            }}
-        /*for (int[] ints : A) {
-            for (int anInt : ints) {
-
-                System.out.print(anInt);
-
+            // Validate input
+            if (rows <= 0 || cols <= 0) {
+                System.out.println("Invalid matrix size! Rows and columns must be positive integers.");
+                return;
             }
-            System.out.println(" ");
-        }
 
-        int [][] transposedMatrix =transpose(A);*/
-        System.out.println("\nTransposed Matrix:");
-        for (int i = 0; i < column; i++) {
-            for (int j = 0; j < row; j++) {
-                System.out.print(A[j][i] + " ");// replace  transposedMatrix [j][i] from line of code
+            // Input matrix elements
+            int[][] matrix = new int[rows][cols];
+            System.out.println("Enter matrix elements:");
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    matrix[i][j] = scanner.nextInt();
+                }
             }
-            System.out.println();
+
+            // Transpose the matrix
+            int[][] transposedMatrix = transpose(matrix);
+
+            // Display transposed matrix
+            System.out.println("\nTransposed Matrix:");
+            for (int i = 0; i < cols; i++) {
+                for (int j = 0; j < rows; j++) {
+                    System.out.print(transposedMatrix[i][j] + " ");
+                }
+                System.out.println();
+            }
         }
-
-        s.close();
-
-        Integer x = 10;
-        Integer y = 10;
-
-        if (x == y) System.out.println("Equal");
-        else System.out.println("Not equal");
     }
 }
